5b837bdfe27dda360b47f99faca6d4abd99b611a
Suman Gurung <suman.grg400@gmail.com>
2025-04-17 21:13:11 +1000

    api key dev settings

--- src/features/auth/authController.ts
+++ src/features/auth/authController.ts
@@ -4,6 +4,8 @@ import expressAsyncHandler from "express-async-handler"
 import AppError from "../../utils/appError"
 import prisma from "../../lib/db"
 import jwt from "jsonwebtoken"
+import { RequestWithUser } from "../../types/customRequest"
+import crypto from "crypto"
 
 export const loginAndSignupUser = expressAsyncHandler(async (req: Request, res: Response) => {
     const { name, email, profile_url } = req.body
@@ -37,4 +39,34 @@ export const loginAndSignupUser = expressAsyncHandler(async (req: Request, res: 
     // Create the token (expires in 1 hour)
     const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '30d' });
     sendResponse({ res, message, data: token })
+})
+
+
+export const getAPIKey = expressAsyncHandler(async (req: RequestWithUser, res: Response) => {
+    const userId = req.userId;
+    if(!userId){
+        throw new AppError("User not found", 404)
+    }
+    const apiKey = await prisma.aPIKey.findFirst({ where: { userId } });
+    sendResponse({ res, message: "API key fetched successfully", data: apiKey })
+})
+
+export const generateAPIKey = expressAsyncHandler(async (req: RequestWithUser, res: Response) => {
+    const userId = req.userId;
+    if(!userId){
+        throw new AppError("User not found", 404)
+    }
+    const hash = crypto
+          .createHmac("sha256", process.env.JWT_SECRET as string)
+          .update(userId + Date.now()) // combine user ID + timestamp
+          .digest("hex");
+
+    
+    try{
+        const apiKey = await prisma.aPIKey.create({ data: { key: hash, isActive: true, userId } });
+        sendResponse({ res, message: "API key generated successfully", data: apiKey })
+    }
+    catch(e){
+        throw new AppError("API key generation failed", 500)
+    }
 })
